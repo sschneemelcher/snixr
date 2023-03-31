dba114659cc4a6305b7a711ed83a3dbd7fc291a2
Simon Schneemelcher <schneemelchercom@gmail.com>
2023-03-31 20:41:59 +0200

    add custom encoding and landing page

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -7,6 +7,10 @@ import (
 
 
 func SetupRoutes(app *fiber.App, rdb *redis.Client) {
+    // Define a route for the root path
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendFile("./public/index.html");
+    })
     app.Post("/api/shorten", CreateLink(rdb))
     app.Get("/:code", RedirectLink(rdb))
     //app.Get("/api/links/:id", GetLink)
--- internal/db/redis.go
+++ internal/db/redis.go
@@ -5,7 +5,7 @@ import "github.com/redis/go-redis/v9"
 func SetupDB() *redis.Client {
     // Setup redis connection
     rdb := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",//"redis:6379",
+        Addr:     "redis:6379",
         Password: "", // no password set
         DB:       0,  // use default DB
     })
